619299de3ee25125a2ceb1aa8dd03ac1ade79d5a
Jadenyoky <53493986+Jadenyoky@users.noreply.github.com>
2023-01-31 19:44:40 +0200

    Add files via upload

---
+++ JS/menu.js
@@ -0,0 +1,19 @@
+const all_page = document.querySelector('.all-page')
+const content_page = document.querySelector('.content-page')
+const menu_page = document.querySelector('.menu-page')
+
+function openPage() {
+    menu_page.style.cssText="width: 300px"
+    content_page.style.cssText="margin-left: 300px"
+    all_page.style.cssText="height: 100vh"
+
+    menu_page.classList.add('ttt')
+    content_page.classList.add('ttt')
+    all_page.classList.add('ttt')
+}
+
+function closePage() {
+    menu_page.style.cssText="width: 0"
+    content_page.style.cssText="margin-left: 0"
+    all_page.style.cssText="height: 0"
+}
